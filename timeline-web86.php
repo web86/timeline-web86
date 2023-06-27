4d745c59b45ef8f5c597a7398bb362827c6cc076
Konstantin Shulyarenko <support@web86.ru>
2023-06-27 10:03:11 +0300

    Update timeline-web86.php

--- timeline-web86.php
+++ timeline-web86.php
@@ -1,33 +1,38 @@
 <?php
+if (!defined('ABSPATH'))
+    exit;
 /*
-Plugin Name: Timeline Web86
+Plugin Name: Web86 Timeline
 Plugin URI: https://web86.ru/timeline-web86
 Description: Simple adaptive timeline (road-map) plugin.
-Version: 1.0
+Version: 1.5
 Author: Konstantin Shulyarenko
 Author URI: https://web86.ru/
 */
 
-function timeline_web86_init()
+function web86_timeline_init()
 {
 
     /**
      * Metabox form template
      */
-    function web86_meta_box_callback($post)
+    function web86_timeline_meta_box_callback($post)
     {
         $value = get_post_meta($post->ID, 'web86_field_name', true);
         if (!is_array($value)) {
             $value = array();
         }
         ?>
-
-        <div id="timeline-web86">
+        
+        <div id="web86_timeline">
+            <?php wp_nonce_field( 'my_action', 'my_nonce' ); ?>
+            <!--<pre><?php // echo print_r($value);?></pre>-->
             <div class="wfieldset">
+                
                 <?php
                 foreach ($value[0] as $key => $group) {
                     echo '<div class="repeatable-fieldset">
-                    <label class="mainlabel"><span class="count">' . ($key + 1) . ')</span> Section name:</label>';
+                    <label class="mainlabel"><span class="count">' . (esc_html($key) + 1) . ')</span> Section name:</label>';
 
                     foreach ($group as $fieldkey => $input_value) {
                         if (!is_array($input_value)) {
@@ -35,7 +40,7 @@ function timeline_web86_init()
                             $placeholder = ($ifif < 2) ? "Section name" : "Section color";
                             $myclass = ($ifif < 2) ? "text-input" : "color-input";
                             $typeinpit = ($ifif < 2) ? "text" : "color";
-                            echo '<input type="' . $typeinpit . '" name="web86_field_name[0][' . ($key + 1) . '][' . $fieldkey . ']" value="' . $input_value . '" class="' . $myclass . '" placeholder="' . $placeholder . '">';
+                            echo '<input type="' . esc_html($typeinpit) . '" name="web86_field_name[0][' . (esc_html($key) + 1) . '][' . esc_html($fieldkey) . ']" value="' . esc_html($input_value) . '" class="' . esc_html($myclass) . '" placeholder="' . esc_html($placeholder) . '">';
                         }
                     }
                     echo '<div class="subfields-container">';
@@ -46,7 +51,7 @@ function timeline_web86_init()
                             foreach ($input_value as $subfield_key => $subfield_value) {
                                 echo '<div class="subfield-container">';
                                 foreach ($subfield_value as $subfield_value_key => $subfield_value_val) {
-                                    echo '<textarea rows="4" name="web86_field_name[0][' . ($key + 1) . '][' . $fieldkey . '][' . $subfield_key . '][' . $subfield_value_key . ']" style="width:100%">' . $subfield_value_val . '</textarea>';
+                                    echo '<textarea rows="4" name="web86_field_name[0][' . (esc_html($key) + 1) . '][' . esc_html($fieldkey) . '][' . esc_html($subfield_key) . '][' . esc_html($subfield_value_key) . ']" style="width:100%">' . esc_html($subfield_value_val) . '</textarea>';
                                 }
                                 echo '<button class="remove-sub-field-row button-secondary">Remove step</button>
                             </div>';
@@ -137,10 +142,10 @@ function timeline_web86_init()
     /**
      * Adding Metabox to the page or post 
      */
-    function web86_add_meta_box()
+    function web86_timeline_add_meta_box()
     {
-        $allowed_page_ids = get_option('timeline_web86_pages', array());
-        $allowed_post_ids = get_option('timeline_web86_posts', array());
+        $allowed_page_ids = get_option('web86_timeline_pages', array());
+        $allowed_post_ids = get_option('web86_timeline_posts', array());
 
         if (in_array(get_the_ID(), $allowed_post_ids) || in_array(get_the_ID(), $allowed_page_ids)) {
             add_meta_box(
@@ -148,7 +153,7 @@ function timeline_web86_init()
                 // Unique identifier of the meta block
                 'Web86 Timeline',
                 // Meta block header
-                'web86_meta_box_callback',
+                'web86_timeline_meta_box_callback',
                 // Callback function for displaying meta block fields
                 array('page', 'post'),
                 // On which screen to show the meta block
@@ -158,9 +163,9 @@ function timeline_web86_init()
             );
         }
     }
-    add_action('add_meta_boxes', 'web86_add_meta_box'); // Adding a meta block
+    add_action('add_meta_boxes', 'web86_timeline_add_meta_box'); // Adding a meta block
 
-    function web86_save_meta_box_data($post_id)
+    function web86_timeline_save_meta_box_data($post_id)
     {
         // Checking access rights.
         if (!current_user_can('edit_post', $post_id)) {
@@ -186,42 +191,89 @@ function timeline_web86_init()
             )
         );
 
-        $web86_field_name = array();
-        foreach ($_POST['web86_field_name'][0] as $group) {
-            $clean_group = array();
-            if (isset($group['field1'])) {
-                $clean_group['field1'] = sanitize_text_field($group['field1']);
-            }
-            if (isset($group['field2'])) {
-                $clean_group['field2'] = sanitize_text_field($group['field2']);
-            }
+        $web86_field_name = array(); // Создаем пустой массив
 
-            foreach ($group as $key => $value) {
-                if (is_array($value)) {
-                    $clean_subfields = array();
-                    foreach ($value as $subfield_key => $subfield_value) {
-                        if (is_array($subfield_value)) {
-                            $clean_subsubfields = array();
-                            foreach ($subfield_value as $subsubfield_key => $subsubfield_value) {
+        // if ( isset( $_POST['my_nonce'] ) && wp_verify_nonce( $_POST['my_nonce'], 'my_action' ) && current_user_can( 'manage_options' ) ) {
 
-                                $clean_subsubfields[$subsubfield_key] = wp_kses($subsubfield_value, $allowed_tags);
-                            }
-                            $clean_subfields[$subfield_key] = $clean_subsubfields;
-                        } else {
+        //     $web86_field_name[0] = array_map( 'sanitize_group', $_POST['web86_field_name'][0] );
+        //     $web86_field_name[1] = wp_kses( $_POST['web86_field_name'][1], $allowed_tags );
 
-                            $clean_subfields[$subfield_key] = wp_kses($subfield_value, $allowed_tags);
-                        }
-                    }
-                    $clean_group[$key] = $clean_subfields;
-                } else {
+        //     // Другой код для обработки данных формы
 
-                    $clean_group[$key] = wp_kses($value, $allowed_tags);
+        // } else {
+        //     // Обработка ошибки, если nonce не прошел проверку или у пользователя нет прав доступа
+        // }
+
+        // // Функция для санитизации каждой группы данных
+        // function sanitize_group( $group ) {
+        //     $clean_group = array();
+        //     if ( isset( $group['field1'] ) ) {
+        //         $clean_group['field1'] = sanitize_text_field( $group['field1'] );
+        //     }
+        //     if ( isset( $group['field2'] ) ) {
+        //         $clean_group['field2'] = sanitize_text_field( $group['field2'] );
+        //     }
+
+        //     foreach ( $group as $key => $value ) {
+        //         if ( is_array( $value ) ) {
+        //             $clean_subfields = array();
+        //             foreach ( $value as $subfield_key => $subfield_value ) {
+        //                 if ( is_array( $subfield_value ) ) {
+        //                     $clean_subsubfields = array();
+        //                     foreach ( $subfield_value as $subsubfield_key => $subsubfield_value ) {
+        //                         $clean_subsubfields[$subsubfield_key] = wp_kses( $subsubfield_value, $allowed_tags );
+        //                     }
+        //                     $clean_subfields[$subfield_key] = $clean_subsubfields;
+        //                 } else {
+        //                     $clean_subfields[$subfield_key] = wp_kses( $subfield_value, $allowed_tags );
+        //                 }
+        //             }
+        //             $clean_group[$key] = $clean_subfields;
+        //         } else {
+        //             $clean_group[$key] = wp_kses( $value, $allowed_tags );
+        //         }
+        //     }
+
+        //     return $clean_group;
+        // }
+
+        if ( isset( $_POST['my_nonce'] ) && wp_verify_nonce( $_POST['my_nonce'], 'my_action' ) ) {
+
+            foreach ($_POST['web86_field_name'][0] as $group) {
+                $clean_group = array();
+                if (isset($group['field1'])) {
+                    $clean_group['field1'] = sanitize_text_field($group['field1']);
                 }
+                if (isset($group['field2'])) {
+                    $clean_group['field2'] = sanitize_text_field($group['field2']);
+                }
+    
+                foreach ($group as $key => $value) {
+                    if (is_array($value)) {
+                        $clean_subfields = array();
+                        foreach ($value as $subfield_key => $subfield_value) {
+                            if (is_array($subfield_value)) {
+                                $clean_subsubfields = array();
+                                foreach ($subfield_value as $subsubfield_key => $subsubfield_value) {
+    
+                                    $clean_subsubfields[$subsubfield_key] = wp_kses($subsubfield_value, $allowed_tags);
+                                }
+                                $clean_subfields[$subfield_key] = $clean_subsubfields;
+                            } else {
+    
+                                $clean_subfields[$subfield_key] = wp_kses($subfield_value, $allowed_tags);
+                            }
+                        }
+                        $clean_group[$key] = $clean_subfields;
+                    } else {
+    
+                        $clean_group[$key] = wp_kses($value, $allowed_tags);
+                    }
+                }
+                $web86_field_name[0][] = $clean_group;
             }
-            $web86_field_name[0][] = $clean_group;
+            $web86_field_name[1] = wp_kses($_POST['web86_field_name'][1], $allowed_tags);
         }
-        $web86_field_name[1] = wp_kses($_POST['web86_field_name'][1], $allowed_tags);
-
 
         // Deleting the fields that were deleted in the form.
         $existing_meta = get_post_meta($post_id, 'web86_field_name', true);
@@ -237,44 +289,44 @@ function timeline_web86_init()
         update_post_meta($post_id, 'web86_field_name', $web86_field_name);
     }
 
-    add_action('save_post', 'web86_save_meta_box_data');
+    add_action('save_post', 'web86_timeline_save_meta_box_data');
 
     // register the fields for the settings page in the menu
-    register_setting('timeline_web86_plugin_settings', 'timeline_web86_pages');
-    register_setting('timeline_web86_plugin_settings', 'timeline_web86_posts');
+    register_setting('web86_timeline_plugin_settings', 'web86_timeline_pages');
+    register_setting('web86_timeline_plugin_settings', 'web86_timeline_posts');
 }
-add_action('admin_init', 'timeline_web86_init');
+add_action('admin_init', 'web86_timeline_init');
 
 
 // Include the script
-function web86_plugin_enqueue_scripts($hook)
+function web86_timeline_plugin_enqueue_scripts($hook)
 {
     if ('post.php' == $hook || 'post-new.php' == $hook || 'page.php' == $hook || 'page-new.php' == $hook) {
         // adding our own scripts and styles
         wp_enqueue_script(
             'web86-plugin-script',
-            plugins_url('/web86-timeline-admin.js', __FILE__),
+            plugins_url('assets/js/web86-timeline-admin.js', __FILE__),
             array('jquery'),
             '1.0.0',
             true
         );
-        wp_enqueue_style('web86-plugin-style', plugins_url('web86-timeline-admin.css', __FILE__));
+        wp_enqueue_style('web86-plugin-style', plugins_url('assets/css/web86-timeline-admin.css', __FILE__));
 
     }
 }
-add_action('admin_enqueue_scripts', 'web86_plugin_enqueue_scripts');
+add_action('admin_enqueue_scripts', 'web86_timeline_plugin_enqueue_scripts');
 
 
 // Create a shortcode
-function web86_plugin_register_shortcodes()
+function web86_timeline_plugin_register_shortcodes()
 {
 
     // inqlude a file with a shortcode
     require_once(plugin_dir_path(__FILE__) . 'shortcode.php');
 
-    add_shortcode('timeline_web86', 'web86_shortcode_function');
+    add_shortcode('web86_timeline', 'web86_timeline_shortcode_function');
 }
-add_action('init', 'web86_plugin_register_shortcodes');
+add_action('init', 'web86_timeline_plugin_register_shortcodes');
 
 // Adding setting page in menu
 add_action('admin_menu', 'web86_timeline_settings');
@@ -286,20 +338,20 @@ function web86_timeline_settings()
     add_options_page(
         'Setting page for Timeline Web86',
         // page title
-        'Timeline',
+        'Web86 timeline',
         // name of the menu item
         'manage_options',
         // access level
-        'timeline_web86_plugin_settings',
+        'web86_timeline_plugin_settings',
         // unique page ID
-        'timeline_web86_settings_page' // the function of displaying the settings page
+        'web86_timeline_settings_page' // the function of displaying the settings page
     );
 }
 
 // Deleting data from the database after deactivating the plugin
 function web86_timeline_plugin_deactivation()
 {
-    delete_option('timeline_web86_pages');
-    delete_option('timeline_web86_posts');
+    delete_option('web86_timeline_pages');
+    delete_option('web86_timeline_posts');
 }
 register_deactivation_hook(__FILE__, 'web86_timeline_plugin_deactivation');
