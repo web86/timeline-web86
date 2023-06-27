1a3f891d3070e2fdb770f4c5f467b43ebac45a40
Konstantin Shulyarenko <support@web86.ru>
2023-06-27 10:01:56 +0300

    Update settings_page.php

--- settings_page.php
+++ settings_page.php
@@ -1,6 +1,9 @@
 <?php
+if (!defined('ABSPATH'))
+  exit;
+
 // Displaying here plugin settings
-function timeline_web86_settings_page()
+function web86_timeline_settings_page()
 {
   // we check that the user has access rights
   if (!current_user_can('manage_options')) {
@@ -13,20 +16,21 @@ function timeline_web86_settings_page()
     </h1>
     <p>1) Choose where do you want the timeline form will be shown</p>
     <form method="post" action="options.php">
-      <?php settings_fields('timeline_web86_plugin_settings'); ?>
-      <?php do_settings_sections('timeline_web86_plugin_settings'); ?>
-      <label for="timeline_web86_pages">Select Pages:</label>
-      <select id="timeline_web86_pages" name="timeline_web86_pages[]" multiple>
+
+      <?php settings_fields('web86_timeline_plugin_settings'); ?>
+      <?php do_settings_sections('web86_timeline_plugin_settings'); ?>
+      <label for="web86_timeline_pages">Select Pages:</label>
+      <select id="web86_timeline_pages" name="web86_timeline_pages[]" multiple>
         <?php
         $pages = get_pages();
         foreach ($pages as $page) {
-          $selected = in_array($page->ID, get_option('timeline_web86_pages', array())) ? 'selected' : '';
-          echo '<option value="' . esc_attr($page->ID) . '" ' . $selected . '>' . esc_html($page->post_title) . '</option>';
+          $selected = in_array($page->ID, get_option('web86_timeline_pages', array())) ? 'selected' : '';
+          echo '<option value="' . esc_attr($page->ID) . '" ' . esc_html($selected) . '>' . esc_html($page->post_title) . '</option>';
         }
         ?>
       </select>
-      <label for="timeline_web86_posts">Select Posts:</label>
-      <select id="timeline_web86_posts" name="timeline_web86_posts[]" multiple>
+      <label for="web86_timeline_posts">Select Posts:</label>
+      <select id="web86_timeline_posts" name="web86_timeline_posts[]" multiple>
         <?php
         $posts = get_posts(
           array(
@@ -35,8 +39,8 @@ function timeline_web86_settings_page()
           )
         );
         foreach ($posts as $post) {
-          $selected = in_array($post->ID, get_option('timeline_web86_posts', array())) ? 'selected' : '';
-          echo '<option value="' . esc_attr($post->ID) . '" ' . $selected . '>' . esc_html($post->post_title) . '</option>';
+          $selected = in_array($post->ID, get_option('web86_timeline_posts', array())) ? 'selected' : '';
+          echo '<option value="' . esc_attr($post->ID) . '" ' . esc_html($selected) . '>' . esc_html($post->post_title) . '</option>';
         }
         ?>
       </select>
@@ -44,16 +48,16 @@ function timeline_web86_settings_page()
     </form>
     <p>2) Place this shortcode to the page or post in editor or into template:</p>
     <p style="font-weight:700">
-      [timeline_web86]
+      [web86_timeline]
     </p>
   </div>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" />
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+  <link rel="stylesheet" href="<?php echo plugins_url('assets/css/select2.min.css', __FILE__); ?>" />
+  <script src="<?php echo plugins_url("assets/js/select2.min.js", __FILE__) ?>"></script>
   <script>
     jQuery(document).ready(function ($) {
-      $('#timeline_web86_pages').select2();
-      $('#timeline_web86_posts').select2();
+      $('#web86_timeline_pages').select2();
+      $('#web86_timeline_posts').select2();
     });
   </script>
   <?php
